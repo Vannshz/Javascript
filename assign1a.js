d2ea2ef32ee667c82d6e1d47e0ccd46fc54040c4
Vannsh <vannsh.nijhawan@zemosolabs.com>
2022-05-12 15:18:45 +0530

    my commit

---
+++ assign1a.js
@@ -0,0 +1,20 @@
+ 
+
+
+  vannsh =() =>
+{
+    return 'My name is ';
+}
+
+  Name=(user,func) =>
+{
+    const message=func();
+       console.log(`${message} ${user}`);
+   
+}
+
+Name('Vansh',vannsh);
+Name('Dhananjay',vannsh);
+
+Name('Shikhar',vannsh);
+
---
+++ assign1b.js
@@ -0,0 +1,8 @@
+vannsh =(firstName,lastName) =>
+{
+let a=firstName;
+let b=lastName;
+console.log(a.charAt(0) + b.charAt(0));
+}
+
+vannsh('Vannsh','Nijhawna');
